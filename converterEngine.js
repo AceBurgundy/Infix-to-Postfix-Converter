fd3b8fcd4cd06277ba4912f4dea9b8e16864dcba
AceBurgundy <Samadriansabalo99@gmail.com>
2022-09-16 19:45:17 +0800

    added showStack, topOfStack and convert.

--- converterEngine.js
+++ converterEngine.js
@@ -23,7 +23,52 @@ class Converter {
         }
     }
 
-    peek() {
-        return this.current
+    showStack() {
+        return this.stack;
+    }
+
+    topOfStack() {
+        return this.stack[this.stack.length - 1]
+    }
+
+    convert(expression) {
+        let convertedExpression = []
+        const text = [...new String(expression)]
+
+        text.forEach(currentElementInTheExpression => {
+            // + returns true if value is number
+            if (+currentElementInTheExpression) {
+                convertedExpression.push(currentElementInTheExpression)
+            }
+            if (currentElementInTheExpression == '(') {
+                this.pushToStack(currentElementInTheExpression)
+            }
+            if (currentElementInTheExpression == ')') {
+                while (this.topOfStack() != '(') {
+                    convertedExpression.push(this.popFromStack())
+                }
+                // pops "("
+                this.popFromStack()
+            }
+            if (Object.keys(this.operators).includes(currentElementInTheExpression)) {
+                if (this.operators[this.topOfStack()] >= this.operators[currentElementInTheExpression]) {
+                    while (this.operators[this.topOfStack()] >= this.operators[currentElementInTheExpression] && Object.keys(this.operators).includes(this.topOfStack())) {
+                        convertedExpression.push(this.popFromStack())
+                    }
+                    this.pushToStack(currentElementInTheExpression)
+                } else {
+                    this.pushToStack(currentElementInTheExpression)
+                }
+            }
+            // if (currentElementInTheExpression == this.showStack()) {
+            //     // convertedExpression.push(this.popFromStack())
+            //     console.log(currentElementInTheExpression);
+            //     convertedExpression.push(currentElementInTheExpression)
+            // }
+        });
+
+        console.log("current stack " + this.showStack().join());
+        console.log("current top " + this.topOfStack());
+        return convertedExpression;
     }
 }
